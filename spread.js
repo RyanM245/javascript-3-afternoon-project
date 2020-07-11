19d617a24fb33a190ee13c519ace71c98aece3a3
Ryan Milne <ryanmilne101@gmail.com>
2020-07-11 11:50:30 -0600

    push 3

--- spread.js
+++ spread.js
@@ -18,7 +18,7 @@ let product = {
 }
 
 // code here
-
+const productCopy = {...product}
 
 
 
@@ -29,7 +29,7 @@ let product = {
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // code here
-
+const colorsCopy = Object.assign({}, colors)
 
 
 
@@ -44,6 +44,7 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
+const result = addNums(4,6,10)
 
 
 ////////// PROBLEM 4 //////////
@@ -54,3 +55,5 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+
+  const combinedArrays = [...arr1,...arr2]
